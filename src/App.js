bbe08ae13e30b0ac6694413232755da859fc8da1
bemrekalkan <bemre.kalkan@gmail.com>
2023-01-13 10:54:48 +0300

    portfolio section added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   return (
    <>
     <Navbar/>
+    
+    {/* PROFILE CARD STARTED */}
     <div className="h-screen">
       <div className="w-2/4 m-auto mt-10 mb-3">
         <h1 className='text-5xl mb-3'>Test Tester</h1>
-        <p className="text-2xl text/cyan-900 ml-2">Frontend Developer</p>
+        <p className="text-2xl text/cyan-900 ml-2">Fullstack Developer</p>
       </div>
 
       <div className="flex space-between w-2/4 m-auto py-5 border rounded-sm px-3 shadow">
@@ -19,6 +21,89 @@ function App() {
         </div>
       </div>
     </div>
+    {/* PROFILE CARD FINISHED */}
+
+
+    {/* EDUCATION CARD STARTED */}
+    <div className='h-screen'>
+      <div className='mb-5 mx-5'>
+        <h1 className='text-5xl mb-3'>My Education</h1>
+        <p className="text-lg text-cyan-900 ">Here is the education that I have received.</p>
+      </div>
+
+      <div className="grid grid-cols-3 gap-4 mx-5">
+        <div className="border rounded-sm p-3 shadow">
+          <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>School, Computer Science</h3>
+          <h5 className="py-2">2010-2013</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, nam autem eius ipsa quis reprehenderit voluptatem explicabo sunt ratione deleniti.</p>
+        </div>
+        <div className="border rounded-sm p-3 shadow">
+          <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>School, Computer Science</h3>
+          <h5 className="py-2">2010-2013</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, nam autem eius ipsa quis reprehenderit voluptatem explicabo sunt ratione deleniti.</p>
+        </div>
+        <div className="border rounded-sm p-3 shadow">
+          <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>School, Computer Science</h3>
+          <h5 className="py-2">2010-2013</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, nam autem eius ipsa quis reprehenderit voluptatem explicabo sunt ratione deleniti.</p>
+        </div>
+      </div>
+    </div>
+    {/* EDUCATION CARD FINISHED */}
+
+    {/* WORK EXPERIENCE STARTED */}
+    <div className='h-screen'>
+      <div className='mb-5 mx-5'>
+        <h1 className='text-5xl mb-3'>My Work Experience</h1>
+        <p className="text-lg text-cyan-900 ">Here is some of my most recent relevant work experience.</p>
+      </div>
+
+      <div className="grid grid-cols-3 gap-4 mx-5">
+        <div className="border rounded-sm p-3 shadow">
+          <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>Company, Fullstack Developer</h3>
+          <h5 className="py-2">2010-2013</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, nam autem eius ipsa quis reprehenderit voluptatem explicabo sunt ratione deleniti.</p>
+        </div>
+        <div className="border rounded-sm p-3 shadow">
+          <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>Company, Fullstack Developer</h3>
+          <h5 className="py-2">2010-2013</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, nam autem eius ipsa quis reprehenderit voluptatem explicabo sunt ratione deleniti.</p>
+        </div>
+        <div className="border rounded-sm p-3 shadow">
+          <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>Company, Fullstack Developer</h3>
+          <h5 className="py-2">2010-2013</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, nam autem eius ipsa quis reprehenderit voluptatem explicabo sunt ratione deleniti.</p>
+        </div>
+      </div>
+    </div>
+    {/* WORK EXPERIENCE FINISHED */}
+
+    {/* PORTFOLIO STARTED */}
+    <div className='h-screen'>
+      <div className='mb-5 mx-5'>
+        <h1 className='text-5xl mb-3'>My Portfolio</h1>
+        <p className="text-lg text-cyan-900 ">Take a look at some of my most recent projects that I have built and the linked code on GitHub.</p>
+      </div>
+
+      <div className="grid grid-cols-3 gap-4 mx-5">
+        <div className="border rounded-sm p-3 shadow">
+          <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>Title</h3>
+          <h5 className="py-2 text-blue-500 hover:text-700 transition cursor-pointer">View Code</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, nam autem eius ipsa quis reprehenderit voluptatem explicabo sunt ratione deleniti.</p>
+        </div>
+        <div className="border rounded-sm p-3 shadow">
+          <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>Title</h3>
+          <h5 className="py-2 text-blue-500 hover:text-700 transition cursor-pointer">View Code</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, nam autem eius ipsa quis reprehenderit voluptatem explicabo sunt ratione deleniti.</p>
+        </div>
+        <div className="border rounded-sm p-3 shadow">
+          <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>Title</h3>
+          <h5 className="py-2 text-blue-500 hover:text-700 transition cursor-pointer">View Code</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, nam autem eius ipsa quis reprehenderit voluptatem explicabo sunt ratione deleniti.</p>
+        </div>
+      </div>
+    </div>
+    {/* PORTFOLIO FINISHED */}
    </>
   );
 }
